518944fc66465205c92b976dc0297ac7bf0b039a
retrofuturejosh <joshuadsohn@gmail.com>
2020-12-07 12:21:18 -0500

    [adds] burrough to config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   phoneNumber: '+15551234567', //PHONE NUMBER (TO RECEIVE NOTIFICATION)
   zipCode: '10019',
-  twilioNumber: '+15551234567' // TWILIO PHONE NUMBER
+  twilioNumber: '+15551234567', // TWILIO PHONE NUMBER
+  burrough: 'Manhattan' // MUST BE: "Bronx", "Brooklyn", "Manhattan", "Queens", or "Staten Island"
 }
