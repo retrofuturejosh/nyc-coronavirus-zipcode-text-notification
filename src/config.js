34db014de06d71249327720197441477f19c0b96
retrofuturejosh <joshuadsohn@gmail.com>
2020-12-08 16:07:43 -0500

    [updates] config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   phoneNumber: '+15551234567', //PHONE NUMBER (TO RECEIVE NOTIFICATION)
   zipCode: '10019',
-  twilioNumber: '+15551234567', // TWILIO PHONE NUMBER
+  twilioNumber: '+15551234567', // TWILIO PHONE NUMBER (from Twilio Dev Dashboard)
   borough: 'Manhattan' // MUST BE: "Bronx", "Brooklyn", "Manhattan", "Queens", or "Staten Island"
 }
